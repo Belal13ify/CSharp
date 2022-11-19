f88c959e235c08e8c68e3df4fbd1d92f69fb07a4
belal13ify <belal_ph13@yahoo.com>
2022-11-19 22:04:36 +0200

    Variables

--- Program.cs
+++ Program.cs
@@ -3,4 +3,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("What is your name?!");
 string? name = Console.ReadLine();
 Console.WriteLine("Welcome " + name + ", How are you?!");
+
+
+int num1;
+int num2;
+
+Console.Write("Please input the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+            
+int result = num1 * num2;
+Console.WriteLine("The result is " + result);
+
 Console.ReadKey();
